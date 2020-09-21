6364bbb11850033f00e90b89bc3e1846a5a0d84b
alalli25 <alalli14@outlook.com>
2020-09-21 02:29:55 -0700

    Uploading final module 10 assignment

---
+++ app.py
@@ -0,0 +1,28 @@
+# set up dependencies
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scraping
+
+# set up flask
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# define the route for the HTML page
+@app.route("/")
+def index():
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)    
+
+# add scraping route to code (acts as button)
+@app.route("/scrape")
+def scrape():
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update({}, mars_data, upsert=True)
+   return "Scraping Successful!"
+
+if __name__ == "__main__":
+   app.run()
